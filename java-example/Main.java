88372ccd01802c64e7836055ad232323bd32a2ff
Shravan Nagarajan <srishravan14@gmail.com>
2023-08-25 22:14:40 +0530

    Added Java example

---
+++ java-example/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String args[]) {
+    System.out.println("Hello from Java! docker build");
+    // System.out.println("Hello from Java! OS: " + System.getProperty("os.name"));
+  }
+}
